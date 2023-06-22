207a500cb33de1bab7b882391f07ba15b4340d81
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-06-22 18:38:12 +0900

    load bootstrap and jquery files

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -6,6 +6,8 @@ import (
 )
 
 func StartMainserver() error {
+	files := http.FileServer(http.Dir(config.Config.Static))
+	http.Handle("/static/", http.StripPrefix("/static/", files))
 	http.HandleFunc("/", top)
 	// 第二引数のnilはデフォルトのマルチプレックスを使用することの宣言、基本はデフォルトのマルチプレっクスを使用する
 	return http.ListenAndServe(":"+config.Config.Port, nil)
