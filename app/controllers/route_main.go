b7e812268e1523e4c8a47296a573fc1a40ef8e98
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-06-22 19:05:56 +0900

    standardize template file

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,16 +1,10 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
-	"text/template"
 )
 
 // この関数の引数はパターン
 func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/templates/top.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	t.Execute(w, "hello")
+	generateHTML(w, "hello", "layout", "top")
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,9 +1,24 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
 	"todo_app/config"
 )
+
+// ...stringは可変長引数
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	// Mustはテンプレートをキャッシュしておくことで効率的に処理できるようにしている、引数のParseFilesがエラーの時、パニックで処理止めれる
+	templates := template.Must(template.ParseFiles(files...))
+	// defineで定義したテンプレートの読み込み、1引数にレスポンスライター、2にテンプレートネーム
+	templates.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainserver() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
