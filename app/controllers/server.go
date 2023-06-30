218d89cba5ede3959ec08b3f0d7c02b13b280c90
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-06-30 20:37:16 +0900

    create update action

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"regexp"
+	"strconv"
 	"text/template"
 	"todo_app/app/models"
 	"todo_app/config"
@@ -33,9 +35,32 @@ func session(writer http.ResponseWriter, request *http.Request) (sess models.Ses
 	return sess, err
 }
 
+var validPath = regexp.MustCompile("^/todos/(edit|save|update|delete)/([0-9]+)$")
+
+// リクエストURL内のidを取得するためのメソッド（最初はパターンとして覚える）
+func parseURL(fn func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// validPathとURLが一致した部分をスライスで取得
+		q := validPath.FindStringSubmatch(r.URL.Path)
+		if q == nil {
+			http.NotFound(w, r)
+			return
+		}
+		// q[0]がtodos、q[1]がedit、q[2]が1みたいな感じでスライスに入っている
+		qi, err := strconv.Atoi(q[2])
+		if err != nil {
+			http.NotFound(w, r)
+		}
+
+		// ここでいうfnはtodoEditのこと（ParseURLが呼ばれその後にtodoEditが呼ばれる）
+		fn(w, r, qi)
+	}
+}
+
 func StartMainserver() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
+	// パスの最後に/をつけない時、パスが完全に一致しないと処理をハンドラーに渡すことができない
 	http.HandleFunc("/", top)
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
@@ -44,6 +69,9 @@ func StartMainserver() error {
 	http.HandleFunc("/todos", index)
 	http.HandleFunc("/todos/new", todoNew)
 	http.HandleFunc("/todos/save", todoSave)
+	// パスの最後に/をつけると、パスが完全に一致しなくても処理をハンドラーに渡すことができる
+	http.HandleFunc("/todos/edit/", parseURL(todoEdit))
+	http.HandleFunc("/todos/update/", parseURL(todoUpdate))
 	// 第二引数のnilはデフォルトのマルチプレックスを使用することの宣言、基本はデフォルトのマルチプレっクスを使用する
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
