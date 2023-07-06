3eaf50badefff451e9aa02a7f0ae48fd10c451e1
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-07-06 17:32:29 +0900

    apply jit mode for tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./app/views/templates/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
